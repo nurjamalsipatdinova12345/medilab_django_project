031eeb254dfb3155efa9ed025595571be0e18c0d
Nelly <nurjamalsipatdinova@gmail.com>
2026-03-10 14:01:41 +0500

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rr0694y33$1vkr^g@l*_tx0h85$wox(ym0qvq!sm_95z!=vvw$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['https://medilab-project.onrender.com', 'localhost', '127.0.0.1:8000']
 
 # Application definition
 
